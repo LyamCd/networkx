5c9ed32f03637e6bed7cfa59323212c6217a2238
Lyamcd <lyam.chardey@gmail.com>
2025-07-12 12:34:48 +0200

    Added missing file `__init__.py`

--- networkx/conftest.py
+++ networkx/conftest.py
@@ -208,6 +208,7 @@ needs_scipy = [
     "algorithms/assortativity/pairs.py",
     "algorithms/bipartite/matrix.py",
     "algorithms/bipartite/spectral.py",
+    "algorithms/bipartite_ged/__init__.py",
     "algorithms/bipartite_ged/costfunctions.py",
     "algorithms/bipartite_ged/solvers.py",
     "algorithms/bipartite_ged/bpged_utils.py",
